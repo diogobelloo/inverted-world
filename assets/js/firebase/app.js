605afef360c4ae35d64afc1198555929d83152c4
diogo belo dos santos <diogobelo69@gmail.com>
2022-08-30 21:00:19 -0300

    up

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,20 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCtQvsI89v49glvQS9cb8Es9wwd-q_e5N8",
+    authDomain: "projeto-teste-3c191.firebaseapp.com",
+    projectId: "projeto-teste-3c191",
+    storageBucket: "projeto-teste-3c191.appspot.com",
+    messagingSenderId: "329588367849",
+    appId: "1:329588367849:web:e1cc88e002934223adab78"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
+  export default app
---
+++ assets/js/firebase/hellfire-clube.js
@@ -0,0 +1,10 @@
+import app from "./app.js"
+import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js'
+
+export async function subscribeToHellfireClub(subscription){
+  
+    const db = getFirestore(app)
+   const hellfireClubeColection = collection(db, 'hellfire-clube')
+   const docRef = await addDoc(hellfireClubeColection, subscription)
+   return docRef.id
+}
